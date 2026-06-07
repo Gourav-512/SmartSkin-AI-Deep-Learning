b9db04876a824821a7053bd4344d4a9f78deec2e
Gourav Salunkhe <137747550+Gourav-512@users.noreply.github.com>
2026-06-07 11:07:55 +0530

    Reformat generate_labels.py for clarity
    Reformat generate_labels.py for clarity

--- generate_labels.py
+++ generate_labels.py
@@ -16,8 +16,10 @@ image_folder = 'dataset/1000_images'
 available_imgs = os.listdir(image_folder)
 available_imgs = [f.replace(".jpg", "") for f in available_imgs]
 
+
 # Filter metadata for available images
 df_filtered = df[df['image_id'].isin(available_imgs)]
+
 
 # Save labels.csv
 output_csv = 'dataset/labels.csv'
