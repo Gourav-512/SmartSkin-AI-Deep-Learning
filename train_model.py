4ddab12edf59ebebc8bf994d8959b58704d9119a
Gourav Salunkhe <137747550+Gourav-512@users.noreply.github.com>
2026-08-25 22:39:35 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -15,6 +15,7 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 IMAGE_DIR = "dataset/1000_images"
 LABELS_CSV = "dataset/labels.csv"
 
+
 # Load CSV
 df = pd.read_csv(LABELS_CSV)
 
