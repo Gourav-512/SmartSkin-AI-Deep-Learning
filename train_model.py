f6cc7803f5e7306262172badfdfb09b62288715b
Gourav Salunkhe <137747550+Gourav-512@users.noreply.github.com>
2026-09-23 23:46:55 +0530

    Refactor train_model.py for clarity and structure

--- train_model.py
+++ train_model.py
@@ -73,4 +73,4 @@ model.fit(
 
 print("✅ Training complete. Model saved as: saved_models/skin_model_1000.h5 ")
 
-#RAG system Will Make It Happen Smoothly.
+#RAG system Will Make It Happen Smoothly.so Whats In Your Brain to Starting A New Video Or Get Stuck On Wherever You are 
